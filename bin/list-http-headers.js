66df2a396d78b277135cbfd74aac7f9f72145478
Austin Wright <aaa@bzfx.net>
2018-04-18 17:43:43 -0700

    Don't wrap header names in headers table

--- bin/list-http-headers.js
+++ bin/list-http-headers.js
@@ -21,7 +21,7 @@ console.log('	<body>');
 console.log('		<main id="main-content">');
 
 console.log('<h1>HTTP Headers</h1>');
-console.log('<table>');
+console.log('<table class="table-http-headers">');
 console.log('<thead>');
 console.log('<tr><th>Header Name</th><th>Direction</th><th>Description</th></tr>');
 console.log('</thead><tbody>');
