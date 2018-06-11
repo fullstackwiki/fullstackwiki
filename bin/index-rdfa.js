56944942803cce62fb92798ad2a56fefc58d15a8
Austin Wright <aaa@bzfx.net>
2018-06-11 03:43:10 -0700

    Use the "rdfa" package

--- bin/index-rdfa.js
+++ bin/index-rdfa.js
@@ -1,16 +1,23 @@
-var parse = require('rdfa-parser')
-var fs = require('fs');
-var path = require('path');
+
+var rdfa = require('rdfa');
 
 var files = process.argv.slice(2);
-var triples = [];
-files.forEach(function(v){
-	var body = fs.readFileSync(v);
-	var fullpath = encodeURI(path.resolve(v));
-	if(fullpath[0]!='/') fullpath = '/' + fullpath;
-	var uri = 'file://'+fullpath;
-	var parsed = parse.parseRDFa(body, uri);
-	for (var i = 0; i < parsed.length; i++) {
-		console.log(parsed[i].toString());
+
+var parse = rdfa.parse;
+
+var fs = require('fs');
+var DOMParser = require('xmldom').DOMParser;
+
+var baseURI = 'http://example.com/';
+
+files.forEach(function(filepath){
+	try {
+		//console.error(filepath);
+		var inputContents = fs.readFileSync(filepath, 'UTF-8');
+		var document = new DOMParser().parseFromString(inputContents, 'text/xml');
+		var result = parse(baseURI, document);
+		console.log(result.outputGraph.toArray().map(function(t){ return t.toString()+'\n'; }).join(''));
+	}catch(err){
+		console.error(err.stack);
 	}
 });
