229c6fac13e4320fa8f22a19ae8e99a7a1b8825d
Austin Wright <aaa@bzfx.net>
2018-10-12 15:22:40 -0700

    Verify meta-charset is defined

--- test/boilerplate.js
+++ test/boilerplate.js
@@ -28,11 +28,18 @@ function testHTMLFilepath(filepath){
 }
 
 function testHTMLDocument(document){
+	// Verify i18n information is set
 	assert(document.documentElement.hasAttribute('lang'));
 	assert(document.documentElement.hasAttribute('dir'));
+	// Verify namespaces are defined
 	assert.equal(document.documentElement.getAttribute('xmlns'), "http://www.w3.org/1999/xhtml");
 	assert.equal(document.documentElement.getAttribute('xmlns:w'), "http://fullstack.wiki/ns/");
+	// Verify the <title/> tag exists and has text content
 	assert(document.getElementsByTagName('title')[0].textContent.length);
+	// Verify the <meta charset="UTF-8" /> tag exists
+	assert(Array.prototype.slice.call(document.getElementsByTagName('meta')).filter(function(e){
+		return e.tagName=='meta' && e.hasAttribute('charset') && e.getAttribute('charset')==='UTF-8';
+	}).length);
 }
 
 describe('Test HTML documents', function(){
