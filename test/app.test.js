8b641404efeee60eef053d47f0015726483a2e36
Austin Wright <aaa@bzfx.net>
2019-03-26 12:41:04 -0700

    Add tests for HTTP server itself

---
+++ test/app.test.js
@@ -0,0 +1,73 @@
+
+var createServer = require('http').createServer;
+var assert = require('assert');
+
+var writeMessage = require('../../dive-httpd/test/util.js').writeMessage;
+var lib = require('../../dive-httpd/index.js');
+var app = require('../app.js');
+
+describe('Server', function(){
+	var server;
+	before(function(){
+		server = createServer(lib.handleRequest.bind(null, app));
+	});
+	after(function(){
+		server = undefined;
+	})
+	it('404 Not Found', function(){
+		// Choose any URI that doesn't have a definition
+		return writeMessage(server, [
+			'GET /some-path-that-does-not-exist HTTP/1.1',
+			'Host: fullstack.wiki',
+		]).then(function(res){
+			assert(res.toString().match(/^HTTP\/1.1 404 /));
+		});
+	});
+	it('/ (landing page)', function(){
+		return writeMessage(server, [
+			'GET /index HTTP/1.1',
+			'Accept: application/xhtml+xml',
+			'Host: fullstack.wiki',
+		]).then(function(res){
+			assert(res.toString().match(/^HTTP\/1.1 200 /));
+			assert(res.toString().match(/^Content-Type: application\/xhtml\+xml$/m));
+			assert(res.toString().match(/<h1>Welcome to Fullstack.wiki<\/h1>/));
+		});
+	});
+	it('/index', function(){
+		return writeMessage(server, [
+			'GET /index HTTP/1.1',
+			'Host: fullstack.wiki',
+		]).then(function(res){
+			assert(res.toString().match(/^HTTP\/1.1 200 /));
+			assert(res.toString().match(/^Content-Type: application\/xhtml\+xml$/m));
+			assert(res.toString().match(/<h1>Welcome to Fullstack.wiki<\/h1>/));
+		});
+	});
+	it('/http/index.md', function(){
+		// Choose any URI that is a Markdown file that is read from the filesystem
+		return writeMessage(server, [
+			'GET /http/index.md HTTP/1.1',
+			'Host: fullstack.wiki',
+		]).then(function(res){
+			assert(res.toString().match(/^HTTP\/1.1 200 /));
+			assert(res.toString().match(/^Content-Type: text\/markdown$/m));
+			assert(res.toString().match(/^# /m));
+		});
+	});
+	it('/index.src.xml', function(){
+		return writeMessage(server, [
+			'GET /index.src.xml HTTP/1.1',
+			'Host: fullstack.wiki',
+			'Connection: close',
+		]).then(function(res){
+			assert(res.toString().match(/^HTTP\/1.1 200 /));
+			assert(res.toString().match(/^Content-Type: application\/xml$/m));
+			assert(res.toString().match(/<h1>Welcome to Fullstack.wiki<\/h1>/));
+		});
+	});
+	it('/graph.ttl');
+	it('/graph.nt');
+	it('/graph.nq');
+	it('/search-index.js');
+});
